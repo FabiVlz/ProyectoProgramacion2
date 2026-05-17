48b915308c7650c34b9de4534d32229349d0e611
Fabian <fabivlzmun@gmail.com>
2026-05-16 22:34:49 -0700

    musica
    cada vez que entres sale una cancion aleatoria, creo qeu cuando termina esa cancion se reproduce otra cancion aleatria, toca calrlo con ams canciones xd

--- src/main/java/com/mycompany/juego/Musica.java
+++ src/main/java/com/mycompany/juego/Musica.java
@@ -1,10 +1,11 @@
 package com.mycompany.juego;
 
 import java.io.BufferedInputStream;
+import java.util.Random;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-
+import javax.sound.sampled.LineEvent;
 /**
  *
  * @author Bilsox
@@ -12,9 +13,20 @@ import javax.sound.sampled.Clip;
 public class Musica {
 
     private static Clip clip;
+    private static Random random = new Random();
 
-    public static void reproducirMusica(String ruta) {
+    private static String[] canciones = {
+        "/musica/Canción de FIFA 22 Mobile ⧸ FIFA 22.wav",
+        "/musica/John Newman - Love Me Again (Lyrics).wav",
+    };
+
+    public static void reproducirAleatoria() {
         try {
+            detenerMusica();
+
+            int indice = random.nextInt(canciones.length);
+            String ruta = canciones[indice];
+
             AudioInputStream audio = AudioSystem.getAudioInputStream(
                 new BufferedInputStream(
                     Musica.class.getResourceAsStream(ruta)
@@ -23,7 +35,15 @@ public class Musica {
 
             clip = AudioSystem.getClip();
             clip.open(audio);
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
+
+            clip.addLineListener(evento -> {
+                if (evento.getType() == LineEvent.Type.STOP) {
+                    if (clip.getMicrosecondPosition() >= clip.getMicrosecondLength()) {
+                        reproducirAleatoria();
+                    }
+                }
+            });
+
             clip.start();
 
         } catch (Exception e) {
@@ -32,8 +52,9 @@ public class Musica {
     }
 
     public static void detenerMusica() {
-        if (clip != null && clip.isRunning()) {
+        if (clip != null) {
             clip.stop();
+            clip.close();
         }
     }
 }
